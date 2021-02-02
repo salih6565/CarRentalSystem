7e58c954e7bab2bd6c8248e97a7b6444eb127403
kara99551 <kara99551@gmail.com>
2021-02-02 14:14:57 +0300

    Proje dosyası ekle.

---
+++ Business/Concrete/CarManager.cs
@@ -0,0 +1,22 @@
+﻿using Business.Abstarct;
+using DataAccess.Abstarct;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class CarManager : ICarManager
+    {
+        ICarDal carManager;
+        public CarManager(ICarDal carDal)
+        {
+            carManager = carDal;
+        }
+        public List<Car> GetAll()
+        {
+            return carManager.GetAll();
+        }
+    }
+}
---
+++ DataAccess/Concrete/InMemoryCarDal/InMemoryCarDal.cs
@@ -0,0 +1,56 @@
+﻿using DataAccess.Abstarct;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemoryCarDal
+{
+    public class InMemoryCarDal : ICarDal
+    {
+        List<Car> _carS;
+        public InMemoryCarDal()
+        {
+            _carS = new List<Car>()
+            {
+                new Car{Id=1,ColorId=1,ModelYear=2000,DailyPrice=35000,BrandId=1,Description="çıtır hasarlı araç"},
+                new Car{Id=2,ColorId=2,ModelYear=2003,DailyPrice=85000,BrandId=2,Description="taksi çıkması"},
+                new Car{Id=3,ColorId=2,ModelYear=2005,DailyPrice=45000,BrandId=3,Description="3000 km de araç"},
+            };
+        }
+
+        public List<Car> GetAll()
+        {
+            return _carS;
+        }
+        public void Update(Car car)
+        {
+            Car CarToUpdate;
+            CarToUpdate = _carS.SingleOrDefault(c => c.Id == car.Id);
+            car.Id = CarToUpdate.Id;
+            car.ColorId = CarToUpdate.ColorId;
+            car.ModelYear = CarToUpdate.ModelYear;
+            car.Description = CarToUpdate.Description;
+            car.DailyPrice = CarToUpdate.DailyPrice;
+            car.BrandId = CarToUpdate.BrandId;
+
+        }
+        public List<Car> GetById(int BrandId)
+        {
+            return _carS.Where(c => c.BrandId == BrandId).ToList();
+        }
+
+        public void Add(Car car)
+        {
+            _carS.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            Car CarToDelete = null;
+            CarToDelete = _carS.SingleOrDefault(c => c.Id == car.Id);
+            _carS.Remove(CarToDelete);
+        }
+    }
+}
